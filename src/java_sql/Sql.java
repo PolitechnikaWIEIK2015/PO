5899990d61893bf2e619c8dc767d24e7882d1d28
hawk <hawk@Dell.home>
2015-06-20 22:44:20 +0200

    Dodanie plików

---
+++ src/java_sql/Sql.java
@@ -0,0 +1,27 @@
+package java_sql;
+
+public class Sql {
+
+	protected static int id_czujnik;
+	protected static String nazwa_czujnik;
+	protected static int typ_czujnik;
+	protected static int stanAc_czujnik;
+	protected static int stanMin_czujnik;
+	protected static int stanMax_czujnik;
+	protected static int data_czujnik;
+	
+	protected static int id_przkaznik;
+	protected static String nazwa_przekaznik;
+	protected static int stanAc_przekaznik;
+	protected static int dataZmiany_przekaznik;
+	protected static int zrudloZmiany_przekaznik;
+	
+	protected static int id_alarm;
+	protected static int data_alarm;
+	protected static int godzina_alarm;
+	protected static int powod_alarm;
+	protected static int zrudlo_alarm;
+	
+	
+	
+}
